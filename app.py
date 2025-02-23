35719521120f826f48abb390e45a5161db192465
datamagnet-dotcom <sainandan2.mn@gmail.com>
2025-02-23 13:29:17 +0530

    app.py

--- app.py
+++ app.py
@@ -57,7 +57,6 @@ st.markdown(custom_css, unsafe_allow_html=True)
 
 # ✅ MongoDB Configuration
 MONGO_URI = "mongodb://sainandan3mn:1234@cluster0-shard-00-00.ik5xa.mongodb.net:27017,cluster0-shard-00-01.ik5xa.mongodb.net:27017,cluster0-shard-00-02.ik5xa.mongodb.net:27017/?ssl=true&replicaSet=atlas-6p2mwc-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
-"
 client = pymongo.MongoClient(MONGO_URI)
 db = client["hospital_db"]
 collection = db["patients"]
