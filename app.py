2985ed5bba73df997b3f6a7a72d8b450a37a28d2
datamagnet-dotcom <sainandan2.mn@gmail.com>
2025-02-25 00:58:06 +0530

    app.py

--- app.py
+++ app.py
@@ -118,17 +118,11 @@ custom_css = """
 st.markdown(custom_css, unsafe_allow_html=True)
 
 # ✅ Display Karexpert logo
-st.markdown(
-    """
+st.markdown("""
     <div class="logo-container">
-        <img src="https://raw.githubusercontent.com/datamagnet-dotcom/medai/main/Karexpert.png" 
-             alt="Karexpert Logo" 
-             style="max-width: 300px; height: auto; display: block; margin: auto;">
+        <img src="https://raw.githubusercontent.com/datamagnet-dotcom/medai/main/Karexpert.png" alt="Karexpert Logo"> 
     </div>
-    """,
-    unsafe_allow_html=True
-)
-
+""", unsafe_allow_html=True)
 
 # ✅ MongoDB Configuration
 MONGO_URI = "mongodb://sainandan3mn:5855@cluster0-shard-00-00.ik5xa.mongodb.net:27017,cluster0-shard-00-01.ik5xa.mongodb.net:27017,cluster0-shard-00-02.ik5xa.mongodb.net:27017/?ssl=true&replicaSet=atlas-6p2mwc-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
